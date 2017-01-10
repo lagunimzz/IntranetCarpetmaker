86ab71c3514d230739c56cb59548bd6a8f59d98d
Thanawat Leetrakulnamchai <lagunimzz@gmail.com>
2017-01-10 23:25:32 +0700

    เพิ่มเยอะแยะเลยงับ

--- src/app/repairs/repair-list/repair-list.component.ts
+++ src/app/repairs/repair-list/repair-list.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Repair } from '../shared/repair.model';
 import { RepairService } from '../shared/repair.service';
 import { NgbPaginationConfig } from '@ng-bootstrap/ng-bootstrap';
 import { Auth } from '../../core/auth.service';
+import { ModalDirective } from 'ng2-bootstrap';
 
 @Component({
   selector: 'repair-list',
-  templateUrl: './repair-list.component.html'
+  templateUrl: './repair-list.component.html',
+  styleUrls: ['./repair-list.component.css']
 })
 export class RepairListComponent implements OnInit {
   repairs: Repair[] = [];
@@ -18,7 +20,7 @@ export class RepairListComponent implements OnInit {
   }
 
   isAdmin() {
-    if(this.auth.userProfile['user_metadata']['department'] == 'Admin'){
+    if (this.auth.userProfile['user_metadata']['department'] == 'Admin') {
       return true;
     } else {
       return false;
@@ -35,5 +37,40 @@ export class RepairListComponent implements OnInit {
       data => this.repairs = data,
       error => console.log(Error)
       );
+  }
+
+  @ViewChild('staticModal') public staticModal:ModalDirective;
+   selectedRepairNo:string;
+   public showChildModal(selectedRepairNo:string):void {
+    this.selectedRepairNo = selectedRepairNo;
+    this.staticModal.show();
+  }
+
+  public max: number = 10;
+  public rate: number = 1;
+
+  public overStar: number;
+  public percent: number;
+
+  public ratingStates: any = [
+    { stateOn: 'fa fa-star fa-6', stateOff: 'fa fa-star-o' },
+    { stateOn: 'fa fa-star', stateOff: 'fa fa-star-o' },
+    { stateOn: 'fa fa-star', stateOff: 'fa fa-star-o' },
+    { stateOn: 'fa fa-star', stateOff: 'fa fa-star-o' },
+    { stateOn: 'fa fa-star', stateOff: 'fa fa-star-o' },
+    { stateOn: 'fa fa-star', stateOff: 'fa fa-star-o' },
+    { stateOn: 'fa fa-star', stateOff: 'fa fa-star-o' },
+    { stateOn: 'fa fa-star', stateOff: 'fa fa-star-o' },
+    { stateOn: 'fa fa-star', stateOff: 'fa fa-star-o' },
+    { stateOn: 'fa fa-star', stateOff: 'fa fa-star-o' }
+  ];
+
+  public hoveringOver(value: number): void {
+    this.overStar = value;
+    this.percent = 100 * (value / this.max);
+  };
+
+  public resetStar(): void {
+    this.overStar = void 0;
   }
 };
